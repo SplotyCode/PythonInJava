36cb3577e660447ae03bee0a82140aa40e958c87
david <davidscandurra@gmail.com>
2018-07-29 21:54:10 +0200

    add any & all functions

--- src/main/java/me/david/pythoninjava/Global.java
+++ src/main/java/me/david/pythoninjava/Global.java
@@ -1,5 +1,7 @@
 package me.david.pythoninjava;
 
+import me.david.pythoninjava.utils.StreamUtils;
+
 public class Global {
 
     public static int abs(int number) {
@@ -16,6 +18,14 @@ public class Global {
 
     public static float abs(float number) {
         return Math.abs(number);
+    }
+
+    public static boolean all(Iterable<Boolean> iterable) {
+        return StreamUtils.stream(iterable).allMatch(bool -> bool);
+    }
+
+    public static boolean any(Iterable<Boolean> iterable) {
+        return StreamUtils.stream(iterable).anyMatch(bool -> bool);
     }
 
 }
---
+++ src/main/java/me/david/pythoninjava/utils/StreamUtils.java
@@ -0,0 +1,19 @@
+package me.david.pythoninjava.utils;
+
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class StreamUtils {
+
+    public static <T> Stream<T> stream(Iterable<T> iterable) {
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(
+                        iterable.iterator(),
+                        Spliterator.ORDERED
+                ), false
+        );
+    }
+
+}
