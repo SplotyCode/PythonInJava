daee792a9c9b3eb8ac5cf4ecb66d8cc1ee4aa5fc
david <davidscandurra@gmail.com>
2018-07-29 21:49:57 +0200

    add abs function

--- src/main/java/me/david/pythoninjava/Global.java
+++ src/main/java/me/david/pythoninjava/Global.java
@@ -2,6 +2,20 @@ package me.david.pythoninjava;
 
 public class Global {
 
+    public static int abs(int number) {
+        return Math.abs(number);
+    }
 
+    public static long abs(long number) {
+        return Math.abs(number);
+    }
+
+    public static double abs(double number) {
+        return Math.abs(number);
+    }
+
+    public static float abs(float number) {
+        return Math.abs(number);
+    }
 
 }
